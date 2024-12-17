c92a3d0b1b1547ffb259a653261b81fe98ddea56
Mustapha Mohammed <mustaphamohammed0507@gmail.com>
2024-12-17 01:10:58 -0800

    configured dotenv for server port

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
+
+const PORT = process.env.PORT || 4100
 
 app.get("/", (req, res) => {
   return res.status(200).send("Api working perfectly");
 });
 
-app.listen(4100, () => {
-  console.log("server running sucessfully at http://localhost:4100");
+app.listen(PORT, () => {
+  console.log(`server running sucessfully at http://localhost:${PORT}`);
 });
