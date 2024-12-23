581d94edd1ba59007d35a05310c6a0ded8740901
tech-gecko <w0nderzz2001@gmail.com>
2024-12-23 18:56:20 +0100

    Update routes/

--- src/routes/reminderRoutes.js
+++ src/routes/reminderRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router(); // Create a new router instance
 
 // Endpoints prefix: api/v1/reminders (DON'T REPEAT THIS PART IN THE ENDPOINTS AGAIN)
 // Define routes
-router.post("/"); // Route for adding a new reminder for the user
-router.get("/:id"); // Route for retrieving the user's reminder (with reminder ID in URL)
-router.get("/"); // // Route for retrieving all the user's reminders
-router.put("/:id"); // Route for updating the user's reminder (with reminder ID in URL)
-router.delete("/:id"); // Route for deleting the user's reminder (with reminder ID in URL)
-router.delete("/"); // Route for deleting all the user's reminders
+router.route("/")
+  .post() // Route for adding a new reminder for the user
+  .get() // // Route for retrieving all the user's reminders
+  .delete(); // Route for deleting all the user's reminders
+
+router.route("/:id")
+  .get() // Route for retrieving the user's reminder (with reminder ID in URL)
+  .put() // Route for updating the user's reminder (with reminder ID in URL)
+  .delete(); // Route for deleting the user's reminder (with reminder ID in URL)
 
 export default router; // Export the router
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router(); // Create a new router instance
 
 // Endpoints prefix: api/v1/users (DON'T REPEAT THIS PART IN THE ENDPOINTS AGAIN)
 // Define routes
-router.get("/me"); // Route for fetching a user's profile details
-router.put("/me"); // Route for updating a user's profile details
-router.delete("/me"); // Route for deleting a user's account and all associated data
+router.route("/me")
+  .get() // Route for fetching a user's profile details
+  .put() // Route for updating a user's profile details
+  .delete(); // Route for deleting a user's account and all associated data
 
 export default router; // Export the router
